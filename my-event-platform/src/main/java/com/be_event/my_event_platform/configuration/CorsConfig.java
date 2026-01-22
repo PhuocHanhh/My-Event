a5c52b09688ad0f9e0f8f95cfc8c98b195f41335
Phuoc Hanh <phanthiphuochanh04@gmai.com>
2026-01-22 22:34:27 +0700

    add Configuration

---
+++ my-event-platform/src/main/java/com/be_event/my_event_platform/configuration/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.be_event.my_event_platform.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:5500"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
+
---
+++ my-event-platform/src/main/java/com/be_event/my_event_platform/configuration/VNPayConfig.java
@@ -0,0 +1,15 @@
+package com.be_event.my_event_platform.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "vnpay")
+@Data
+public class VNPayConfig {
+    private String tmnCode;
+    private String hashSecret;
+    private String vnpUrl;
+    private String vnpReturnUrl;
+}
